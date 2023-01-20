f7b6cbe9f78b5bac889a84de2675b86b100afa13
Navod21 <navod@commercialtp.com>
2023-01-20 18:35:26 +0530

    initiated project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const App = () => {
+  return (
+    <View>
+      <Icon name="ios-person" size={30} color="#4F8EF7" />
+    </View>
+  );
+};
+
+export default App;
